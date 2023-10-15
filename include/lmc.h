045925670db3d92d13d337c2bacb1ab8ee6db8ac
Peter0x44 <peter0x44@disroot.org>
2023-10-16 00:16:04 +0100

    Implement initial GetLine function

---
+++ include/lmc.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef struct
+{
+	int mailBoxes[100]; // main memory
+	int accumulator;
+	int programCounter;
+	int outbox;
+	bool wantsInput;
+	bool wantsOutput;
+} LMCContext;
+
+typedef struct
+{
+	char* str;
+	ptrdiff_t len;
+} s8;
+
+// Types of errors, could be bad assembly, or something else
+typedef enum
+{
+	lllllll
+} AssemblerError;
+
+typedef enum
+{
+	ERROR_BAD_PC, // ex. type in number that is not in the range (-999, 999)
+	ERROR_HALT // Halt instruction reached - program execution finished
+} RuntimeError;
+// errors that can happen during runtime: example PC value is outside of [0, 99]
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Assemble string assembly into LMCContext code
+AssemblerError Assemble(s8 assembly, LMCContext* code);
+
+// Execute next instruction of code
+RuntimeError Step(LMCContext* code);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/lmc.c
@@ -0,0 +1,92 @@
+#include "lmc.h"
+
+#define S(s) (s8) { (char*)s, (ptrdiff_t)(sizeof(s)-1) }
+
+#ifdef DEBUG
+#define assert(e) if (!(e)) __builtin_trap()
+#else
+#define assert(e)
+#endif
+
+#include <stdio.h>
+
+// function to print s8s for debugging
+void s8Print(s8 s)
+{
+//        printf("\"%.*s\"\n", (int)s.len, s.str);
+	putchar('\"');
+	for (int i = 0; i < s.len; ++i)
+	{
+		if (s.str[i] == '\n')
+		{
+			putchar('\\');
+			putchar('n');
+		}
+		else
+			putchar(s.str[i]);
+	}
+	putchar('\"');
+	putchar('\n');
+}
+
+s8 GetLine(s8 buf)
+{
+	// Skip over any newlines, if they are at the beginning
+	s8 tmp = buf;
+	for (ptrdiff_t i = 0; i < buf.len; ++i)
+	{
+		if (tmp.str[i] == '\n' || tmp.str[i] == '\r')
+		{
+			++buf.str;
+			--buf.len;
+		}
+		else
+			break;
+	}
+
+	// find the next newline, and end the string before it
+	for (char* s = buf.str; s < buf.str + buf.len; ++s)
+	{
+		if (*s == '\n')
+		{
+			s8 ret;
+			ret.str = buf.str;
+			ret.len = s - buf.str;
+			return ret;
+		}
+	}
+	// No newline found - last line or EOF
+	return buf;
+}
+
+AssemblerError Assemble(s8 assembly, LMCContext* code)
+{
+	assert(code);
+	return 0;
+}
+
+
+int main(void)
+{
+	s8 bruh = S("\n\n bruh  \n bruhhh\n ");
+	s8 tmp = GetLine(bruh);
+	s8Print(tmp);
+
+	bruh.len -= tmp.str - bruh.str + tmp.len;
+	bruh.str += tmp.str - bruh.str + tmp.len;
+
+	tmp = GetLine(bruh);
+	s8Print(tmp);
+
+	bruh.len -= tmp.str - bruh.str + tmp.len;
+	bruh.str += tmp.str - bruh.str + tmp.len;
+
+	tmp = GetLine(bruh);
+	s8Print(tmp);
+
+	bruh.len -= tmp.str - bruh.str + tmp.len;
+	bruh.str += tmp.str - bruh.str + tmp.len;
+
+	tmp = GetLine(bruh);
+	s8Print(tmp);
+}
